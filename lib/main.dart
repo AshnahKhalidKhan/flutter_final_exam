d049e5996b0f11ba09494ba6db0d113b7051a936
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-18 14:28:57 +0500

    Firebase setup (actually truly utterly) complete! Alhumdulillah

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_final_exam/firebase_options.dart';
 
 void main() async 
 {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp
+  (
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
@@ -39,16 +43,6 @@ class MyHomePage extends StatefulWidget
 
 class _MyHomePageState extends State<MyHomePage> 
 {
-  int _counter = 0;
-
-  void _incrementCounter() 
-  {
-    setState(() 
-    {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) 
   {
@@ -57,31 +51,6 @@ class _MyHomePageState extends State<MyHomePage>
       (
         title: Text(widget.title),
       ),
-      // body: Center
-      // (
-      //   child: Column
-      //   (
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>
-      //     [
-      //       const Text
-      //       (
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       Text
-      //       (
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headlineMedium,
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      // floatingActionButton: FloatingActionButton
-      // (
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
     );
   }
 }
