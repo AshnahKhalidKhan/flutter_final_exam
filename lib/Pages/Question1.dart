9f963105dee33e61f9575c44c3e425e5f158c2c0
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-12-22 21:26:10 +0500

    Bus

---
+++ lib/Pages/Question1.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_final_exam/Bloc/product_bloc.dart';
+import 'package:flutter_final_exam/Bloc/product_events.dart';
+import 'package:flutter_final_exam/Bloc/product_states.dart';
+import 'package:flutter_final_exam/Core/Repository/product_repository.dart';
+import 'package:intl/intl.dart';
+
+
+class Question1 extends StatefulWidget 
+{
+  final DataRepository dataRepository;
+  const Question1({super.key, required this.dataRepository});
+
+  @override
+  State<Question1> createState() => _Question1State();
+}
+
+class _Question1State extends State<Question1> 
+{
+  late DataBloc _dataBloc;
+  int totalAmount = 0;
+
+  @override
+  void initState() 
+  {
+    super.initState();
+    _dataBloc = DataBloc(dataRepository: widget.dataRepository);
+    _dataBloc.add(const fecthDataEvent_DataEventsFile());
+  }
+  
+  Widget build(BuildContext context) 
+  {
+    return Scaffold
+    (
+      backgroundColor: const Color.fromARGB(255, 224, 224, 224),
+      appBar: AppBar
+      (
+        backgroundColor: const Color.fromARGB(255, 224, 224, 224),
+        leading: Icon(Icons.arrow_back_rounded),
+        actions: [Padding(padding: EdgeInsets.all(20.0), child: Icon(Icons.more_horiz_rounded))],
+      ),
+      body: Column
+      (
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: 
+        [
+          Padding
+          (
+            padding: EdgeInsets.all(30.0),
+            child: Row
+            (
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: 
+              [
+            
+                Column
+                (
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: 
+                  [
+                    Text(DateFormat('EEEE').format(DateTime.now()).toUpperCase(), style: TextStyle(color: Colors.grey),),
+                    Text(DateTime.now().day.toString() + ' ' + DateFormat('MMM').format(DateTime.now()), style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),),
+                    
+                  ],
+                ),
+                Text(
+                  // ---- Funcion jo value return karay
+                  '\$' + totalAmount.toString(),
+                  style: TextStyle
+                  (
+                    color: Colors.blue,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container
+          (
+            width: double.infinity,
+            
+            decoration: BoxDecoration
+            (
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            // child: 
+            // ListTile
+            // (
+            //   leading: NetworkImage
+            //   (
+            //     url
+            //   ),
+            //   title: ,
+            //   subtitle: ,
+            // )
+            child: Center
+            (
+              child: _BlocBuilderBananayWalaFunction()
+              // child: Text('Haha')
+            )
+
+
+
+
+
+            // child: Text(DateFormat('yMd').format(DateTime.now())),
+
+          )
+        ],
+      ),
+    );
+  }
+
+  BlocBuilder<DataBloc, DataState> _BlocBuilderBananayWalaFunction() 
+  {
+    return BlocBuilder
+    (
+      bloc: _dataBloc,
+      builder: (_, DataState state)
+      {
+        if (state is EmptyState)
+        {
+          return const Center(child: Text('EmptyState'));
+        }
+        if (state is ErrorState)
+        {
+          return const Text('Error');
+        }
+        if (state is LoadingState)
+        {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (state is SuccessOrLoadedState)
+        {
+          return _ListViewBananayWalaFunction(state);
+        }
+        return const Text('You forgot to cater some state');
+      },
+    );
+  }
+
+  ListView _ListViewBananayWalaFunction(SuccessOrLoadedState state) 
+  {
+    return ListView.builder
+    (
+      itemCount: state.data.length,
+      itemBuilder: (BuildContext context, int i)
+      {
+        var obj = state.data[i];
+        return Card
+        (
+          // child: ListTile
+          // (
+          //   leading: const Icon(Icons.person),
+          //   title: Text(obj.title ?? '--'),
+          //   subtitle: Text(obj.id.toString()),
+          //   trailing: CircleAvatar(backgroundColor: Colors.black, foregroundColor: Colors.red,),
+          // ),
+          child: ListTile
+          (
+            leading: ClipRRect
+            (
+              borderRadius: BorderRadius.circular(20.0),//or 15.0
+              child: Container
+              (
+                height: 20.0,
+                width: 20.0,
+                child: Expanded(child: Image.network(obj.images.toString())),
+              ),
+            ),
+            title: Text(obj.title, style: TextStyle(),),
+            subtitle: ,
+          )
+        );
+      }
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_final_exam/Pages/Question1.dart';
 import 'package:flutter_final_exam/Pages/Question2.dart';
 import 'package:flutter_final_exam/Pages/SignIn.dart';
 import 'package:http/http.dart' as http;
@@ -9,29 +10,55 @@ import 'package:flutter_final_exam/Core/network.dart';
 import 'package:flutter_final_exam/Pages/LoginPage.dart';
 import 'package:flutter_final_exam/firebase_options.dart';
 
-// void main() async 
+void main() async 
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp
+  (
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  final DataRepository dataRepository = DataRepository
+  (
+    dataAPIClient: DataAPIClient(httpClient: http.Client()),
+    firebaseFirestore: FirebaseFirestore.instance
+  );
+  //runApp(const MyApp());
+  runApp
+  (
+    MyApp(dataRepository: dataRepository)
+  );
+}
+
+class MyApp extends StatelessWidget 
+{
+  final DataRepository dataRepository;
+  const MyApp({super.key, required this.dataRepository});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
+      title: 'Flutter Demo',
+      // home: LoginPage(dataRepository: dataRepository)
+      // home: SignIn()
+
+
+
+      home: Question1(dataRepository: dataRepository),
+      // home: Question2()
+    );
+  }
+}
+
+// void main() 
 // {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp
-//   (
-//     options: DefaultFirebaseOptions.currentPlatform
-//   );
-//   final DataRepository dataRepository = DataRepository
-//   (
-//     dataAPIClient: DataAPIClient(httpClient: http.Client()),
-//     firebaseFirestore: FirebaseFirestore.instance
-//   );
-//   //runApp(const MyApp());
-//   runApp
-//   (
-//     MyApp(dataRepository: dataRepository)
-//   );
+//   runApp(const MyApp());
 // }
 
 // class MyApp extends StatelessWidget 
 // {
-//   final DataRepository dataRepository;
-//   const MyApp({super.key, required this.dataRepository});
+//   const MyApp({super.key});
 
 //   @override
 //   Widget build(BuildContext context) 
@@ -45,25 +72,3 @@ import 'package:flutter_final_exam/firebase_options.dart';
 //     );
 //   }
 // }
-
-void main() 
-{
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget 
-{
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) 
-  {
-    return MaterialApp
-    (
-      title: 'Flutter Demo',
-      // home: LoginPage(dataRepository: dataRepository)
-      // home: SignIn()
-      home: Question2()
-    );
-  }
-}
